943a25244cf2ee4f6153d4464666525889dd856b
udaykiran199715 <udaykiran199715@gmail.com>
2022-05-19 01:43:14 +0530

    Added inputs code in SI problem

--- C_Language_Chapter 1/Chapter 1/My_Practice/simple_interest.c
+++ C_Language_Chapter 1/Chapter 1/My_Practice/simple_interest.c
@@ -3,6 +3,13 @@
 int main()
 {
     int principal = 1000, rate = 10, years = 5;
+    printf("Enter prinicpal amount: \n");
+    scanf("%d", &principal);
+    printf("Enter rate of interest: \n");
+    scanf("%d", &rate);
+    printf("Enter number of years: \n");
+    scanf("%d", &years);
+
     int simpleInterest = (principal * rate * years) / 100;
     printf("Simple Interst is: %d", simpleInterest);
     return 0;
